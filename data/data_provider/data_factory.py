bea5b5748e21acb685a8e000c308812f42493798
ForJustYou <ForJustYou@outlook.com>
2026-01-07 20:10:17 +0800

    add datasets Estonian

--- data/data_provider/data_factory.py
+++ data/data_provider/data_factory.py
@@ -1,10 +1,11 @@
-from data.data_provider.data_loader import Dataset_SHEERM,Dataset_PSHE,Dataset_CityLearn
+from data.data_provider.data_loader import Dataset_SHEERM,Dataset_PSHE,Dataset_CityLearn,Dataset_Estonian
 from torch.utils.data import DataLoader
 
 data_dict = {
     'SHEERM': Dataset_SHEERM,
     'PSHE': Dataset_PSHE,
     'CityLearn': Dataset_CityLearn,
+    'Estonian': Dataset_Estonian,
 }
 
 
@@ -35,7 +36,7 @@ def data_provider(args, flag):
         seasonal_patterns=args.seasonal_patterns,
         cycle=args.cycle,
     )
-    if args.data == 'SHEERM' or args.data == 'PSHE' or args.data == 'CityLearn':
+    if args.data == 'SHEERM' or args.data == 'PSHE' or args.data == 'CityLearn' or args.data == 'Estonian':
         data_kwargs['pretreatment'] = args.pretreatment
         data_kwargs['cycle'] = args.cycle
 
